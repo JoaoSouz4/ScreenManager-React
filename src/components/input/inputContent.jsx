420dc36f630bc8ca86437949b3c4585f5f20c86f
João Souza <joaosouz8@gmail.com>
2023-08-13 14:54:05 -0300

    personalização dos cards

--- src/components/input/inputContent.jsx
+++ src/components/input/inputContent.jsx
@@ -1,6 +1,6 @@
 export function InputContent({children}){
     return(
-        <div className = 'p-2 flex w-full h-full rounded-lg border-2 border-ghover items-center'>
+        <div className = 'p-2 flex w-full h-full rounded-lg border-2 border-ghover items-center focus:outline-1 focus:outline-primary'>
             {children}
         </div>
     )
--- src/pages/SearchScreen/componentCard.jsx
+++ src/pages/SearchScreen/componentCard.jsx
@@ -30,42 +30,49 @@ export function ComponentCard(props){
             <div 
                 onClick={() => setModalState({...modalState, isOpen: true})}
             > 
-                <div className = "flex flex-col gap-2 w-full">
+                <div className = "flex flex-col gap-2 w-full h-full">
                     <div className = 'flex gap-4 items-center justify-between '>
                         <label className = 'text-xl text-black'><strong>{name}</strong></label>
                         <label className = ' p-0 text-[rgb(32,32,32,55%)] '>{brand}</label>
                     </div>
 
-                    <div className = ' border rounded-xl p-4 border-ghover flex flex-col gap-3'>
-                        <div className = 'flex justify-between'>
-                            <label className='text-gray2'>Paralela:</label>
-                            <span>
-                                <div className = 'flex gap-2'>
-                                    <div className = 'text-primary'>R${prices.parallel}</div>
-                                    <div className = ' px-1  text-[white] border   bg-gray2 rounded'><strong>R$ {prices.parallel + laborState}</strong></div>
-                                </div>
-                            </span>
-                        </div>
+                    <div className = ' border rounded-xl p-4 border-ghover flex flex-col gap-3' >
+                        { prices.parallel &&
+                            <div className = 'flex justify-between border rounded-xl p-3 border-ghover'>
+                                <label className='text-gray2'>Paralela:</label>
+                                <span>
+                                    <div className = 'flex gap-2'>
+                                        <div className = 'text-primary'>R${prices.parallel}</div>
+                                        <div className = ' px-1  text-[white] border   bg-gray2 rounded'><strong>R$ {prices.parallel + laborState}</strong></div>
+                                    </div>
+                                </span>
+                            </div>
+                        }
 
-                        <div className = 'flex justify-between'>
-                            <label className='text-gray2'>Intermediária:</label>
-                            <span>
-                                <div className = 'flex gap-2'>
-                                    <div className = 'text-primary'>R${prices.medium}</div>
-                                    <div className = ' px-1 text-[white] border  bg-gray2 rounded'><strong>R$ {prices.medium + laborState}</strong></div>
-                                </div>
-                            </span>
-                        </div>
+                        {
+                            prices.medium &&
+                            <div className = 'flex justify-between border rounded-xl p-3 border-ghover'>
+                                <label className='text-gray2'>Intermediária:</label>
+                                <span>
+                                    <div className = 'flex gap-2'>
+                                        <div className = 'text-primary'>R${prices.medium}</div>
+                                        <div className = ' px-1 text-[white] border  bg-gray2 rounded'><strong>R$ {prices.medium + laborState}</strong></div>
+                                    </div>
+                                </span>
+                            </div>
+                        }
 
-                        <div className = 'flex justify-between'>
-                            <label className='text-gray2'>Primeira linha:</label>
-                            <span>
-                                <div className = 'flex gap-2'>
-                                    <div className = 'text-primary'>R${prices.firstLine}</div>
-                                    <div className = ' px-1 text-[white] border  bg-gray2 rounded'><strong>R$ {prices.firstLine + laborState}</strong></div>
-                                </div>
-                            </span>
-                        </div>
+                        { prices.firstLine &&
+                            <div className = 'flex justify-between border rounded-xl p-3 border-ghover'>
+                                <label className='text-gray2'>Primeira linha:</label>
+                                <span>
+                                    <div className = 'flex gap-2'>
+                                        <div className = 'text-primary'>R${prices.firstLine}</div>
+                                        <div className = ' px-1 text-[white] border  bg-gray2 rounded'><strong>R$ {prices.firstLine + laborState}</strong></div>
+                                    </div>
+                                </span>
+                            </div>
+                        }
                     </div>
                 </div>
             </div>
